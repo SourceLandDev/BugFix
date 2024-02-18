1d3b947160bd37ff233eae2733b07c7242c46cff
予纾 <100036558+Hosiyume@users.noreply.github.com>
2024-02-19 00:04:21 +0800

    fix: Fix no bad falling

--- src/NoBadFalling.cpp
+++ src/NoBadFalling.cpp
@@ -1,9 +1,6 @@
 #include <ll/api/memory/Hook.h>
 #include <mc/world/actor/Actor.h>
-#include <mc/world/level/BlockSource.h>
-#include <mc/world/level/block/Block.h>
 #include <mc/world/level/block/BlockLegacy.h>
-#include <mc/world/level/block/utils/VanillaBlockTypeIds.h>
 
 LL_AUTO_TYPE_INSTANCE_HOOK(
     NoBadFallingHook,
@@ -16,15 +13,6 @@ LL_AUTO_TYPE_INSTANCE_HOOK(
     if (!origin(entity)) {
         return false;
     }
-
-    Vec3 const&        pos         = entity.getPosition();
-    BlockSource const& blockSource = entity.getDimensionBlockSourceConst();
-    Block const&       block       = blockSource.getBlock(pos);
-    std::string        blockType   = block.getTypeName();
-    if (blockType == VanillaBlockTypeIds::Water.getString()
-        || blockType == VanillaBlockTypeIds::BigDripleaf.getString()) {
-        return false;
-    }
-
+    entity.remove();
     return true;
 }
