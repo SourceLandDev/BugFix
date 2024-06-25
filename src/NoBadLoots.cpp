810f270a64a64e49cf63d49493a7e0d47e0e2f9b
予纾 <100036558+Yuisyuu@users.noreply.github.com>
2024-06-25 21:26:23 +0800

    feat: Add no bad loots impl

---
+++ src/NoBadLoots.cpp
@@ -0,0 +1,58 @@
+#include <ll/api/memory/Hook.h>
+#include <mc/entity/components/ProjectileComponent.h>
+#include <mc/world/actor/Mob.h>
+#include <mc/world/actor/player/Player.h>
+#include <vector>
+
+bool hited = false;
+
+LL_AUTO_TYPE_INSTANCE_HOOK(
+    ProjectileHitHook,
+    ll::memory::HookPriority::Normal,
+    ProjectileComponent,
+    &ProjectileComponent::onHit,
+    void,
+    Actor&           a2,
+    HitResult const& a3
+) {
+    hited = true;
+    origin(a2, a3);
+    hited = false;
+}
+
+std::vector<Actor*> actors{};
+
+LL_AUTO_TYPE_INSTANCE_HOOK(
+    MobDieHook,
+    ll::memory::HookPriority::Normal,
+    Mob,
+    "?die@Mob@@UEAAXAEBVActorDamageSource@@@Z",
+    void,
+    ActorDamageSource const& a2
+) {
+    if (hited) {
+        actors.push_back(this);
+    }
+    origin(a2);
+}
+
+Player* lastPlayer = nullptr;
+
+LL_AUTO_TYPE_INSTANCE_HOOK(GetLastHurtHook, ll::memory::HookPriority::Normal, Actor, &Actor::getLastHurtByPlayer, Player*) {
+    auto result = origin();
+    auto index  = std::find(actors.begin(), actors.end(), this);
+    if (index != actors.end()) {
+        lastPlayer = result;
+        actors.erase(index);
+    }
+    return result;
+}
+
+LL_AUTO_TYPE_INSTANCE_HOOK(GetSelectedItemHook, ll::memory::HookPriority::Normal, Player, &Player::getSelectedItem, ItemStack const&) {
+    auto result = ItemStack::EMPTY_ITEM;
+    if (lastPlayer != this) {
+        result = origin();
+    }
+    lastPlayer = nullptr;
+    return result;
+}
